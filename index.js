add1b911dc39c7ce6a8386049034635394a3a525
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-11-17 23:04:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const api = {
    key: "5166d6bfe55bc81ec2d7bb78b538650f",
 };
 
+// default weather
+async function defaultWeather(city) {
+   const defaultCity = await fetch(
+      `${api.endpoint}weather?q=${city}&units=metric&appID=${api.key}`
+   );
+   const defaultWeather = await defaultCity.json();
+   displayDefault(defaultWeather);
+}
+function displayDefault(defaultWeather) {
+   let getDefaultCity = document.querySelector("#city");
+   getDefaultCity.innerHTML = `<i class="fa-solid fa-location-dot fa-sm"></i> ${defaultWeather.name}, ${defaultWeather.sys.country}`;
+
+   let defaultTemp = document.querySelector("#temperature");
+   defaultTemp.innerHTML = `${Math.round(defaultWeather.main.temp)}°`;
+
+   let defaultFeelsLike = document.querySelector("#feelsLike");
+   defaultFeelsLike.innerHTML = `Feels ${Math.round(
+      defaultWeather.main.feels_like
+   )}°`;
+
+   let defaultTempVariation = document.querySelector("#variation");
+   defaultTempVariation.innerHTML = `Min ${Math.round(
+      defaultWeather.main.temp_min
+   )}° | Max ${Math.round(defaultWeather.main.temp_max)}°`;
+}
+defaultWeather("Toronto");
+
+// date
 function getDate() {
    const today = new Date();
    const days = [
@@ -37,6 +65,7 @@ function getDate() {
 }
 getDate();
 
+// search
 const input = document.querySelector("#search");
 input.addEventListener("keydown", enter);
 
